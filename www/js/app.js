1136db2b55dc6343cdd3f0af26a4a54bb31dea4d
Circé <circegrand@gmail.com>
2017-03-29 10:57:22 +0200

    CSS3 git commit -m CSS3

--- www/js/app.js
+++ www/js/app.js
@@ -22,6 +22,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.directive', 
     }
   });
 })
+
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('app', {
