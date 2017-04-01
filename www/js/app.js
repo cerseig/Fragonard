2d6cd887bd09e194fc5592ffdac6f1f4b1c0ec69
Circé <circegrand@gmail.com>
2017-04-01 14:28:49 +0200

    Share

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.directive', 'ui.router', 'ngMap'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.directive', 'ui.router', 'ngMap', 'djds4rce.angular-socialshare'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -21,6 +21,10 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.directive', 
       StatusBar.styleDefault();
     }
   });
+})
+
+.run(function($FB){
+  $FB.init('132111483989009');
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
